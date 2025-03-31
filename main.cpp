759aa04a1d889a43bc4e4759bc7faf7be01b47bf
Blake Pennel <blakepennel@ku.edu>
2025-03-30 19:03:52 -0500

    Add files with some decent working methods

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include "matrix.hpp"
+#include <iostream>
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char* argv[]) {
+    // Create a 3x3 matrix for testing
+    Matrix mat(3);
+
+    // Fill it with some values (including a big one)
+    mat.set_value(0, 0, 1);
+    mat.set_value(0, 1, 23);
+    mat.set_value(0, 2, 4567);
+    mat.set_value(1, 0, -89);
+    mat.set_value(1, 1, 100);
+    mat.set_value(1, 2, 7);
+    mat.set_value(2, 0, 42);
+    mat.set_value(2, 1, 0);
+    mat.set_value(2, 2, 5);
+
+    std::cout << "Matrix test output:\n";
+    mat.print_matrix();
+
     return 0;
 }
--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,9 @@
-#include "matrix.hpp"
+#include "matrix.hpp"    // the header for Matrix class
+#include <iostream>      // for std::cout
+#include <iomanip>       // for std::setw
+#include <stdexcept>     // for std::out_of_range
+#include <string>        // for std::string and std::to_string
+#include <vector>        // if not already transitively included
 
 // implementation of functions declared in matrix.hpp goes here
 // function definitions for a class have their names prefixed with
@@ -8,10 +13,45 @@
 
 Matrix::Matrix(size_t N) {
     // initialize an empty NxN matrix
+    // we'll use a list of list of ints for a 2D array (matrix)
+    data = std::vector<std::vector<int>>(N, std::vector<int>(N, 0));
+    // ^ A vector of N rows, each one being a vector of zeros, for example:
+    // For N=4 we get:
+    // [
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0],
+    //   [0, 0, 0, 0]
+    // ]
+}
+
+void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+    if (i >= data.size() || j >= data[i].size()) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    data[i][j] = n;
 }
 
 // e.g. for a member function:
-
 void Matrix::print_matrix() const {
-    // print out the matrix
+    // determine the maximum width needed for alignment
+    int max_width = 0;
+    for (const std::vector<int>& row : data) {
+        for (int val : row) {
+            int len = std::to_string(val).length();
+            if (len > max_width)
+                max_width = len;
+        }
+    }
+
+    // add some padding
+    const int padding = 1;
+    int column_width = max_width + padding;
+
+    for (const std::vector<int>& row : data) {
+        for (int val : row) {
+            std::cout << std::setw(column_width) << val;
+        }
+        std::cout << "\n";
+    }
 }
